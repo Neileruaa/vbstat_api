f35cfdaee2df9fd36a020b63e0be47845d533b62
aurelien <aureliendrey@gmail.com>
2020-08-20 10:06:03 +0200

    test controller

--- src/Controller/MatchController.php
+++ src/Controller/MatchController.php
@@ -4,9 +4,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MatchController extends AbstractController
 {
-
+    /**
+     * @Route("/test")
+     */
+    public function test()
+    {
+        return new Response("TEST");
+    }
 }
